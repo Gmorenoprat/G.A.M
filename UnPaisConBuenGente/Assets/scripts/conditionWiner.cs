872dec7495033a123018ff792deaaeeba30a1170
mariano <marianoa.munguia@gmail.com>
2020-10-26 20:38:50 -0300

    canvas y scene victoria

---
+++ UnPaisConBuenGente/Assets/scripts/conditionWiner.cs
@@ -0,0 +1,22 @@
+﻿using Boo.Lang;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class conditionWiner : MonoBehaviour
+{
+    public GameObject[] gorila;
+    public GameObject[] pinguins;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
