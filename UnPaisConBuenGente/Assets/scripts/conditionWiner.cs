b8c428eb278abf87bc13ffd4e645be192a3ce7ef
mariano <marianoa.munguia@gmail.com>
2020-10-26 21:16:55 -0300

    condicion de victoria
    prueba 1

--- UnPaisConBuenGente/Assets/scripts/conditionWiner.cs
+++ UnPaisConBuenGente/Assets/scripts/conditionWiner.cs
@@ -2,11 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class conditionWiner : MonoBehaviour
+public class conditionWiner : DestroyableComp
 {
     public GameObject[] gorila;
     public GameObject[] pinguins;
+    public int totalLifeGorila;
+    public int totalLifePinguin;
+    public int playersNumber;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +21,18 @@ public class conditionWiner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       // totalLifeGorila = life / playersNumber ;
+
+       // totalLifeGorila = life / playersNumber;
+
+        if(totalLifeGorila >= 0)
+        {
+            SceneManager.LoadScene("winner pinguin");
+        }
+
+        if (totalLifeGorila >= 0)
+        {
+            SceneManager.LoadScene("winner gorila");
+        }
     }
 }
