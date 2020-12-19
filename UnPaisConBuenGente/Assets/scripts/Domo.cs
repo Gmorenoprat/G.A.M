f799d1b256c1c92f3981f6ed6880ac2428c65a6f
gonzalo.morenop <gonzalo.morenop@davinci.edu.ar>
2020-12-19 16:35:38 -0300

    Domo

---
+++ UnPaisConBuenGente/Assets/scripts/Domo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Domo : MonoBehaviour
+{
+    private bool golpeado = false;
+    public Sprite domoRoto;
+    public AudioSource sonidoVidrio;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if(collision.gameObject.layer == 10)
+        {
+            if(golpeado == true)
+            {
+                sonidoVidrio.Play();
+                Destroy(this.gameObject);
+                return;
+            }
+            golpeado = true;
+            this.GetComponent<SpriteRenderer>().sprite = domoRoto;
+            sonidoVidrio.Play();
+        }
+    }
+}
