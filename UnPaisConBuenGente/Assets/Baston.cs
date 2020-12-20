6749b4fabf7284ccbe087d5bc9fa7128cd255a86
mariano <marianoa.munguia@gmail.com>
2020-12-20 19:57:18 -0300

    fix final ultra

---
+++ UnPaisConBuenGente/Assets/Baston.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Baston : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            StartCoroutine(ocultarUnosSegs());
+        }
+    }
+
+    public  IEnumerator ocultarUnosSegs()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        yield return new WaitForSeconds(0.6f);
+        this.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        yield return null;
+    }
+}
