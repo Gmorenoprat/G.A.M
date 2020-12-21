7ec0bff39d5ed95f3abee51f735091ef77c67b55
Ariel <arielferrerxd@gmail.com>
2020-12-21 17:41:33 -0300

    Golpe bastón
    Retoque roce personajes
    el baston no se pega a si mismo

---
+++ UnPaisConBuenGente/Assets/scripts/ForceAddHit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceAddHit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.GetComponent<Rigidbody2D>().AddForce
+            (this.gameObject.transform.up * 80, ForceMode2D.Impulse);
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        collision.gameObject.GetComponent<Rigidbody2D>().AddForce
+            (this.gameObject.transform.up * 80, ForceMode2D.Impulse);
+    }
+}
